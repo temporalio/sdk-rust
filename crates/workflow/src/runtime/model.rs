ce09e78d996d21c7c91e2a33094b484b16b8d53c
Chris Olszewski <chris.olszewski@temporal.io>
2026-06-11 14:49:12 -0400

    feat(sdk): move to boxed errors for WorkflowError variant

--- crates/workflow/src/runtime/model.rs
+++ crates/workflow/src/runtime/model.rs
@@ -247,7 +247,11 @@ impl From<crate::runtime::entry::WorkflowError> for WorkflowTermination {
     fn from(value: crate::runtime::entry::WorkflowError) -> Self {
         match value {
             crate::runtime::entry::WorkflowError::PayloadConversion(err) => Self::from(err),
-            crate::runtime::entry::WorkflowError::Execution(err) => Self::from(err),
+            crate::runtime::entry::WorkflowError::Execution(err) => Self::Failed(
+                temporalio_common_wasm::error::OutgoingWorkflowError::Application(Box::new(
+                    ApplicationFailure::new(err),
+                )),
+            ),
         }
     }
 }
