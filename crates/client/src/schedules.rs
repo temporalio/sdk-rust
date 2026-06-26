49de3e63326edb25fcb25e075dbf8cd8f36c705f
Chris Olszewski <chrisdolszewski@gmail.com>
2026-06-26 08:57:57 -0400

    Apply suggestion from @chris-olszewski

--- crates/client/src/schedules.rs
+++ crates/client/src/schedules.rs
@@ -1769,9 +1769,8 @@ mod tests {
 
     #[tokio::test]
     async fn schedule_action_start_workflow_with_input_into_proto() {
-        use temporalio_client::UntypedWorkflow;
         use temporalio_common::{
-            data_converters::{DataConverter, RawValue},
+            UntypedWorkflow, data_converters::{DataConverter, RawValue},
             protos::temporal::api::common::v1::Payload,
         };
 
