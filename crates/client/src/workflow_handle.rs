ef28befb5618f61df109f6ab19a3f42a1809bc5b
Chris Olszewski <chrisdolszewski@gmail.com>
2026-07-29 11:11:12 -0400

    Apply suggestion from @chris-olszewski

--- crates/client/src/workflow_handle.rs
+++ crates/client/src/workflow_handle.rs
@@ -1309,7 +1309,7 @@ where
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::test_helpers::{FailingCodec, XorCodec};
+    use crate::test_helpers::XorCodec;
     use std::collections::HashMap;
     use temporalio_common::{
         data_converters::DefaultFailureConverter,
